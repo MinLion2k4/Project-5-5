1e1bd0d0ea16cb6f3adb2c21e59a6bbcb0a3e008
MinLion2k4 <135206246+MinLion2k4@users.noreply.github.com>
2024-05-04 19:39:21 +0700

    demo

--- public/product2.js
+++ public/product2.js
@@ -1,16 +1,12 @@
-const menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+const menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-
-
-
-
-let products = localStorage.getItem('products');
+let products = localStorage.getItem("products");
 
 if (products) {
   // If products exist in localStorage, parse them and use them
@@ -18,20 +14,21 @@ if (products) {
   renderProducts(products);
 } else {
   // If products do not exist in localStorage, fetch them
-  fetch('/public/product.json')
-    .then(response => response.json())
-    .then(data => {
+  fetch("/public/product.json")
+    .then((response) => response.json())
+    .then((data) => {
       // Save the fetched products to localStorage for future use
-      localStorage.setItem('products', JSON.stringify(data));
+      localStorage.setItem("products", JSON.stringify(data));
       renderProducts(data);
     })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 }
 
 function renderProducts(products) {
-  const productList = document.getElementById('product-list');
-  const html = products.map(product => {
-    return `
+  const productList = document.getElementById("product-list");
+  const html = products
+    .map((product) => {
+      return `
       <div class="col">
         <div class="card">
           <img src="${product.img}" class="card-img-top" alt="...">
@@ -39,13 +36,23 @@ function renderProducts(products) {
             <h5 class="card-title">${product.Name}</h5>
             <p class="card-text">Miễn phí giao hàng</p>
             <div class="d-flex justify-content-around mb-3">
-              <h3>${parseInt(product.Price).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}/con</h3>
-              <button class="btn btn-primary" data-product-name="${product.Name}" data-product-id="${product.id}" data-product-image="${product.img}" data-product-price="${product.Price}" onclick="addToCart(event)">Mua Ngay</button>
+              <h3>${parseInt(product.Price).toLocaleString("vi-VN", {
+                style: "currency",
+                currency: "VND",
+              })}/con</h3>
+              <button class="btn btn-primary" data-product-name="${
+                product.Name
+              }" data-product-id="${product.id}" data-product-image="${
+        product.img
+      }" data-product-price="${
+        product.Price
+      }" onclick="addToCart(event)">Mua Ngay</button>
             </div>
           </div>
         </div>
       </div>
     `;
-  }).join('');
+    })
+    .join("");
   productList.innerHTML = html;
 }
