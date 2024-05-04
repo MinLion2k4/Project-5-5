b76a493feccc615e15b34a71eceeb611738dcc26
MinLion2k4 <135206246+MinLion2k4@users.noreply.github.com>
2024-05-04 23:17:08 +0700

    Hero

--- admin.js
+++ admin.js
@@ -52,3 +52,43 @@ document.getElementById('addProductForm').addEventListener('submit', (e) => {
   document.getElementById('addProductModal').style.display = 'none';
   window.location.reload();
 });
+
+window.onload = function() {
+  let user = JSON.parse(localStorage.getItem("user"));
+  if (user && user.admin) {
+    let nav = document.getElementById('navbar'); // replace with your navbar id
+    let adminLink = document.createElement('a');
+    adminLink.href = 'admin.html'; // replace with your admin page link
+    adminLink.textContent = 'Quản Lý';
+    adminLink.className = 'active';
+    nav.appendChild(adminLink);
+  }
+}
+document.getElementById('searchBar').addEventListener('input', (e) => {
+  const searchTerm = e.target.value.toLowerCase();
+
+  // Lọc danh sách sản phẩm dựa trên tên sản phẩm
+  const filteredProducts = products.filter(product => product.Name.toLowerCase().includes(searchTerm));
+
+  // Hiển thị danh sách sản phẩm đã lọc
+  renderAdminProductList1(filteredProducts);
+});
+
+const renderAdminProductList1 = (productsToRender) => {
+  const productListHTML = productsToRender.map((product) => {
+    return `
+    <tr>
+    <th scope="row">${product.id}</th>
+    <td><img src="${product.img}" class="img-fluid" alt="..."></td>
+    <td><p>${product.Name}</p></td>
+    <td><p>${parseInt(product.Price).toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}/con</p></td>
+    <td><button class="btn btn-danger" data-id="${product.id}">Xóa</button></td>
+    </tr>
+    `;
+    // ...
+  }).join('');
+  document.getElementById('productList').innerHTML = productListHTML;
+};
+
+// Gọi hàm renderAdminProductList với danh sách sản phẩm ban đầu
+renderAdminProductList(products);
